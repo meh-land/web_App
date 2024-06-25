e845eeda4f50a4764b05345adce1a2f8ef474bcf
YahiaEldakhakhny <yahia4360673@gmail.com>
2024-06-25 14:37:43 +0300

    video streaming link

--- src/Components/Dashboard/Live_Location/LiveLocation.tsx
+++ src/Components/Dashboard/Live_Location/LiveLocation.tsx
@@ -1,8 +1,12 @@
 import { FC } from "react";
 import Card from "../Card/Card";
+import VideoComponent from "./vedio";
 
 const LiveLocation: FC = () => {
-  return <Card Title="Live Location" Content={<h1>Video Stream</h1>} />;
+  return <Card Title="Live Location" Content={
+<><h1>Video Stream</h1> <a href="http://192.168.43.178:8080/stream.html" target="_blank">link</a>
+<VideoComponent targetUrl="http://192.168.43.178:8080/stream.html"/></>
+  } />;
 };
 
 export default LiveLocation;
