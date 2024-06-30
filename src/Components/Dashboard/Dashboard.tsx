480729f8a13097e477c8bdc88034d5c6b10d71a3
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-06-30 06:41:04 +0200

    dashboard adjusted

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,9 +1,6 @@
 import { FC } from "react";
 import ControlPanel from "./Control_Panel/Control_Panel";
 import LiveLocation from "./Live_Location/LiveLocation";
-import MotorsSpeed from "./Motors_Speed/MotorsSpeed";
-import Status from "./Status/Status";
-import Loggers from "./Loggers/Loggers";
 import Header from "./Header/Header";
 import Navbar from "../Navbar/Navbar";
 
@@ -15,20 +12,11 @@ const Dashboard: FC = () => {
       <Header />
       <div className="mx-4">
         <div className="row">
-          <div className="col-lg-8 col-sm-12 order-sm-2 order-lg-1">
+          <div className=" col-12 order-sm-2 order-lg-1">
             <LiveLocation />
-            <MotorsSpeed />
           </div>
-          <div className="col-lg-4 col-sm-12 order-sm-1 order-lg-2">
+          <div className="col-12 order-sm-1 order-lg-2">
             <ControlPanel />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-5">
-            <Status />
-          </div>
-          <div className="col-lg-7">
-            <Loggers />
           </div>
         </div>
       </div>
